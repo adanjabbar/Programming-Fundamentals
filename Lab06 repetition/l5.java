3d0667da8992fd6271010c7e1a3155ff4d2497e5
adanjabbar <adanjabbar1352@gmail.com>
2025-11-24 00:11:28 +0500

    Update l5.java

--- Lab06 repetition/l5.java
+++ Lab06 repetition/l5.java
@@ -1,3 +1,7 @@
+/*Write a program that prompts the user to enter the year and first day of the year and displays the calendar 
+table for the year on the console. For example, if the user entered the year 2013, and 2 for Tuesday, January 
+1, 2013, your program should display the calendar for each month in the year*/
+
 import java.util.Scanner;
 public class l5{
 	public static void main(String[]args){
@@ -40,4 +44,5 @@ System.out.println();
    }
 input.close();
 	}
+
 }
