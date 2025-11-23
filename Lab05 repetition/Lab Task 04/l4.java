f6bd79c8d46a1d8ace86b0bdf957b04463c12f31
adanjabbar <adanjabbar1352@gmail.com>
2025-11-24 00:28:59 +0500

    Create l4.java

---
+++ Lab05 repetition/Lab Task 04/l4.java
@@ -0,0 +1,26 @@
+/*The length of Sequence: Given a sequence of non-negative integers, where each number is written in a 
+separate line. Determine the length of the sequence, where the sequence ends when the integer is equal to 
+0. Print the length of the sequence (not counting the integer 0). The numbers following the number 0 should 
+be omitted. */
+
+import java.util.Scanner;
+public class task4{
+
+public static void main(String[]args){
+        Scanner input=new Scanner(System.in);
+int n, count=0;
+	System.out.println("Enter numbers (0 to end):");
+
+while(true){            //infinite loop ends on break
+	n= input.nextInt();
+
+	if(n==0){
+	break;
+	        }
+	count++;
+	}
+
+System.out.print("The length of sequence is : " + count);
+input.close();
+	}
+}
